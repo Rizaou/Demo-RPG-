57512acd4642e139fd262653aa3c26972dbfe1e1
Deniz Basocak <denizzr@outlook.com>
2022-05-08 11:51:29 +0300

    ADD Suspicious Behaviour
    AI stays a few seconds before moving its guard location.

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using RPG.Combat;
 using RPG.Core;
+using RPG.Movement;
 using UnityEngine;
 
 namespace RPG.Controller
@@ -9,16 +10,24 @@ namespace RPG.Controller
     public class AIController : MonoBehaviour
     {
         [SerializeField] private float chaseDistance = 5f;
+        [SerializeField] private float suspitionTime = 3f;
+
+        private float lastTimeSawPlayer = 0f;
 
         private Fighter fighter;
+        private Mover mover;
         private GameObject player;
         private Health health;
+        private Vector3 guardPosition;
 
         private void Start()
         {
             fighter = GetComponent<Fighter>();
             player = GameObject.FindWithTag("Player");
             health = GetComponent<Health>();
+            mover = GetComponent<Mover>();
+            guardPosition = transform.position;
+
         }
         private void Update()
         {
@@ -27,15 +36,36 @@ namespace RPG.Controller
 
             if (InAttackRangeOfPlayer() && fighter.CanAttack(player))
             {
-                fighter.Attack(player);
-
+                AttackBehaviour();
+                lastTimeSawPlayer = 0f;
+            }
+            else if (!InAttackRangeOfPlayer() && lastTimeSawPlayer >= suspitionTime)
+            {
+                lastTimeSawPlayer += Time.deltaTime;
+                SuspiciousBehaviour();
             }
             else
             {
-                fighter.Cancel();
+                lastTimeSawPlayer += Time.deltaTime;
+                GuardBehaviour();
 
             }
 
+        }
+
+        private void GuardBehaviour()
+        {
+            fighter.Cancel();
+        }
+
+        private void SuspiciousBehaviour()
+        {
+            mover.StartMoveAction(guardPosition);
+        }
+
+        private void AttackBehaviour()
+        {
+            fighter.Attack(player);
         }
 
         private bool InAttackRangeOfPlayer()
@@ -43,6 +73,14 @@ namespace RPG.Controller
 
             float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
             return distanceToPlayer <= chaseDistance;
+        }
+
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, chaseDistance);
+
         }
     }
 }
