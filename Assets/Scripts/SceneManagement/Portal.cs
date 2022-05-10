08b9dd664cf68b6436a334f9d761d3a3adb3c092
Deniz Basocak <denizzr@outlook.com>
2022-05-10 14:35:51 +0300

    UPDATE Portal.cs
    Portal Works properly

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,6 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
-
+using UnityEngine.AI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,13 +9,47 @@ namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
+
+
         [SerializeField] private int sceneToLoad = -1;
+        [SerializeField] private Transform spawnPos;
         private void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.tag == "Player")
             {
-                SceneManager.LoadScene(sceneToLoad);
+                StartCoroutine(Transition());
             }
+        }
+
+        public Transform GetSpawnPoint => spawnPos;
+
+        private IEnumerator Transition()
+        {
+            DontDestroyOnLoad(this.gameObject);
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+            Destroy(gameObject);
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+
+                return portal;
+            }
+
+            return null;
+        }
+
+        private void UpdatePlayer(Portal portal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(portal.GetSpawnPoint.position);
+            player.transform.rotation = portal.GetSpawnPoint.rotation;
+
         }
     }
 
